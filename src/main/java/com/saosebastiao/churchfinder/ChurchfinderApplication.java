4140178a1494c56d9bc423d41f81a0e595512860
Carlos Melo <carlosmelo@hotmail.com.br>
2024-08-23 16:49:00 -0300

    Projeto iniciado

---
+++ src/main/java/com/saosebastiao/churchfinder/ChurchfinderApplication.java
@@ -0,0 +1,13 @@
+package com.saosebastiao.churchfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChurchfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChurchfinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saosebastiao/churchfinder/ChurchfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saosebastiao.churchfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChurchfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
