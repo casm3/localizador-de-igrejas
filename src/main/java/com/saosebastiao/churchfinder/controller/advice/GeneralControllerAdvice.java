d2d1edea53a54eaff3e2ed9cfdfd3289bc3a7273
Carlos Melo <carlosmelo@hotmail.com.br>
2024-08-26 07:10:48 -0300

    creates controller advice

--- src/main/java/com/saosebastiao/churchfinder/controller/advice/GeneralControllerAdvice.java
+++ src/main/java/com/saosebastiao/churchfinder/controller/advice/GeneralControllerAdvice.java
@@ -1,4 +1,16 @@
 package com.saosebastiao.churchfinder.controller.advice;
 
+import com.saosebastiao.churchfinder.service.exception.CustomNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GeneralControllerAdvice {
+  @ExceptionHandler
+  @ResponseStatus(HttpStatus.NOT_FOUND)
+  public String handleNotFoundException(CustomNotFoundException e) {
+    return e.getMessage();
+  }
 }
