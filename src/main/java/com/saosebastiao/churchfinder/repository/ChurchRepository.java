d94e13d3746a43169dca31ddb08bd65512f89bd6
Carlos Melo <carlosmelo@hotmail.com.br>
2024-08-26 07:06:12 -0300

    creates repository

--- src/main/java/com/saosebastiao/churchfinder/repository/ChurchRepository.java
+++ src/main/java/com/saosebastiao/churchfinder/repository/ChurchRepository.java
@@ -1,4 +1,7 @@
 package com.saosebastiao.churchfinder.repository;
 
-public interface ChurchRepository {
+import com.saosebastiao.churchfinder.entity.Church;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChurchRepository extends JpaRepository<Church, Long> {
 }
