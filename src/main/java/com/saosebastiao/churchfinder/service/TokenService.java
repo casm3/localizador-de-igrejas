22fe46370a4047a8739c9eb73bb91e325af5f132
Carlos Melo <carlosmelo@hotmail.com.br>
2024-09-20 16:21:41 -0300

    implements token service

--- src/main/java/com/saosebastiao/churchfinder/service/TokenService.java
+++ src/main/java/com/saosebastiao/churchfinder/service/TokenService.java
@@ -1,6 +1,8 @@
 package com.saosebastiao.churchfinder.service;
 
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.saosebastiao.churchfinder.entity.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +12,19 @@ public class TokenService {
 
   public TokenService(@Value("${church.security.token.secret}") String secret) {
     this.algorithm = Algorithm.HMAC256(secret);
+  }
+
+  public String generateToken(Person person) {
+    return JWT.create().withIssuer("church")
+            .withSubject(person.getUsername())
+            .sign(algorithm);
+  }
+
+  public String validateToken(String token) {
+    return JWT.require(algorithm)
+            .withIssuer("church")
+            .build()
+            .verify(token)
+            .getSubject();
   }
 }
