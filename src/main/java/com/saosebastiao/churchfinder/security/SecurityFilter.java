be4b1b47ec30c7e78360464da46cfa0da0ac1cf7
Carlos Melo <carlosmelo@hotmail.com.br>
2024-09-20 16:27:31 -0300

    implements security filter

--- src/main/java/com/saosebastiao/churchfinder/security/SecurityFilter.java
+++ src/main/java/com/saosebastiao/churchfinder/security/SecurityFilter.java
@@ -1,11 +1,15 @@
 package com.saosebastiao.churchfinder.security;
 
+import com.saosebastiao.churchfinder.service.PersonService;
 import com.saosebastiao.churchfinder.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,8 +21,27 @@ public class SecurityFilter extends OncePerRequestFilter {
   private TokenService tokenService;
   @Autowired
   private PersonService personService;
+
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    String token = recoveryToken(request);
 
+    if (token != null) {
+      String username = tokenService.validateToken(token);
+      UserDetails userDetails = personService.loadUserByUsername(username);
+
+      UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+      SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    filterChain.doFilter(request, response);
+  }
+
+  private String recoveryToken(HttpServletRequest request) {
+    String authorization = request.getHeader("Authorization");
+    if (authorization != null) {
+      return authorization.replace("Bearer ", "");
+    }
+    return null;
   }
 }
