a18f7761a67f8cad45cf544bbbea5940f6378435
Carlos Melo <carlosmelo@hotmail.com.br>
2024-09-20 16:38:54 -0300

    creates and implements DocAuthorization

---
+++ src/main/java/com/saosebastiao/churchfinder/security/DocAuthentication.java
@@ -0,0 +1,16 @@
+package com.saosebastiao.churchfinder.security;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+
+public class DocAuthentication implements OpenApiCustomizer {
+  public final String SCHEME_NAME = "Bearer Auth";
+
+  @Override
+  public void customise(OpenAPI openApi) {
+    openApi.getComponents().addSecuritySchemes(SCHEME_NAME, new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT"));
+    openApi.addSecurityItem(new SecurityRequirement().addList(SCHEME_NAME));
+  }
+}
